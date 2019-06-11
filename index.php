42d0e54c3ff61452b73d144180738b7e5d063b79
denisleshchenko <dan_77@list.ru>
2019-06-11 11:27:06 +0400

    one

--- index.php
+++ index.php
@@ -4,5 +4,25 @@
 </head>
 <body>
 <p>Hello!</p>
+<?php
+
+define("DEFAULT_TEMPLATE_PATH", '/local/templates/.default');
+
+function debug($data){
+    echo '<pre>'.print_r($data,true).'</pre>';
+}
+
+function cut($string, $length){
+$string = mb_substr($string, 0, $length,'UTF-8'); // обрезаем и работаем со всеми кодировками и указываем исходную кодировку
+$position = mb_strrpos($string, ' ', 'UTF-8'); // определение позиции последнего пробела. Именно по нему и разделяем слова
+$string = mb_substr($string, 0, $position, 'UTF-8'); // Обрезаем переменную по позиции
+return $string.' ...';
+}
+
+function resImg($img,$width,$height){
+    $file = CFile::ResizeImageGet($img, array('width'=>$width, 'height'=>$height), BX_RESIZE_IMAGE_EXACT , true);
+    return $file;
+}
+?>
 </body>
 </html>
